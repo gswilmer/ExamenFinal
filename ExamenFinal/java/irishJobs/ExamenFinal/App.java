9aa474087e5c6518c346cc4b3d12f84518e8bb88
gswilmer <wilbertogomezs@gmail.com>
2024-11-23 09:57:43 -0600

    Commint Inicial
    Creacion de Proyecto

---
+++ ExamenFinal/java/irishJobs/ExamenFinal/App.java
@@ -0,0 +1,13 @@
+package irishJobs.ExamenFinal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
